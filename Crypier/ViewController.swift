d2259524742b0680fa6e8a3db1ce00742d3f054c
Batuhan Saygılı <batuhansaygili94@gmail.com>
2018-01-09 22:24:02 +0300

    Initial Commit

---
+++ Crypier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crypier
+//
+//  Created by Batuhan Saygılı on 9.01.2018.
+//  Copyright © 2018 batuhansaygili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
